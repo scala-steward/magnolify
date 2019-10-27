2e8bb569d6dc410180ef4aa0c768041e7b1b4c78
Neville Li <neville@spotify.com>
2019-10-27 19:26:31 -0400

    wip

--- bigquery/src/main/scala/magnolia/bigquery/TableRowType.scala
+++ bigquery/src/main/scala/magnolia/bigquery/TableRowType.scala
@@ -9,34 +9,55 @@ import magnolia.shims._
 import scala.collection.JavaConverters._
 import scala.language.experimental.macros
 
-sealed trait TableRowType[T] extends Converter.Record[T, TableRow] {
-  protected type R = TableRow
-  def apply(r: R): T = from(r)
-  def apply(t: T): R = to(t)
-  override protected def empty: R = new TableRow
-  override protected def from(r: R): T = ???
-  override protected def to(t: T): R = ???
+sealed trait TableRowType[T] extends Converter[T, TableRow] {
+  type R = TableRow
+  def apply(r: R): T = this match {
+    case tc: TableRowRecord[T] => tc.from(r)
+  }
+  def apply(t: T): R = this match {
+    case tc: TableRowRecord[T] => tc.to(t)
+  }
 }
 
-object TableRowType {
-  type Typeclass[T] = TableRowField[T]
+trait TableRowRecord[T] extends TableRowType[T] with Converter.Record[T, TableRow] {
+  override def empty: TableRow = new TableRow
+}
 
-  def combine[T](caseClass: CaseClass[Typeclass, T]): Typeclass[T] = new Typeclass[T] {
-    override protected def from(r: R): T =
-      caseClass.construct(p => p.typeclass.get(r, p.label))
+class TableRowField[V](val getFn: Any => V)(val putFn: V => Any)
+  extends TableRowType[V] with Converter.Field[V, TableRow] {
+  override def get(r: R, k: String): V = getFn(r.get(k))
+  override def put(r: R, k: String, v: V): R = {
+    r.put(k, putFn(v))
+    r
+  }
 
-    override protected def to(t: T): R =
-      caseClass.parameters.foldLeft(empty) { (r, p) =>
-        p.typeclass.put(r, p.label, p.dereference(t))
-        r
+  def imap[U](f: V => U)(g: U => V): TableRowField[U] =
+    new TableRowField[U](v => f(getFn(v)))(v => putFn(g(v)))
+}
+
+object TableRowType extends TableRowTypeImplicits {
+  type Typeclass[T] = TableRowType[T]
+
+  def combine[T](caseClass: CaseClass[Typeclass, T]): Typeclass[T] = new TableRowRecord[T] {
+    override def from(r: R): T = caseClass.construct { p =>
+      p.typeclass match {
+        case tc: TableRowRecord[p.PType] =>
+          val copy = tc.empty
+          copy.putAll(r.get(p.label).asInstanceOf[java.util.Map[String, Any]])
+          copy
+        case tc: TableRowField[p.PType] =>
+          tc.get(r, p.label)
       }
-
-    override def fromField(v: Any): T = {
-      val r = empty
-      r.putAll(v.asInstanceOf[java.util.Map[String, Any]])
-      this.from(r)
     }
-    override def toField(v: T): Any = to(v)
+
+    override def to(t: T): R = caseClass.parameters.foldLeft(empty) { (r, p) =>
+      p.typeclass match {
+        case tc: TableRowRecord[p.PType] =>
+          r.put(p.label, tc.to(p.dereference(t)))
+          r
+        case tc: TableRowField[p.PType] => tc.put(r, p.label, p.dereference(t))
+      }
+    }
   }
 
   def dispatch[T](sealedTrait: SealedTrait[Typeclass, T]): Typeclass[T] = ???
@@ -44,66 +65,58 @@ object TableRowType {
   implicit def apply[T]: TableRowType[T] = macro Magnolia.gen[T]
 }
 
-sealed trait TableRowField[V]
-  extends TableRowType[V]
-  with Converter.Field[V, TableRow] { self =>
-  override def get(r: R, k: String): V = fromField(r.get(k))
-  override def put(r: R, k: String, v: V): Unit = r.put(k, toField(v))
+trait TableRowTypeImplicits {
+  def at[V](f: Any => V)(g: V => Any): TableRowType[V] = new TableRowField[V](f, g)
 
-  def fromField(v: Any): V
-  def toField(v: V): Any
-
-  def imap[U](f: V => U)(g: U => V): TableRowField[U] = new TableRowField[U] {
-    override def fromField(v: Any): U = f(self.fromField(v))
-    override def toField(v: U): Any = self.toField(g(v))
-  }
-}
-
-object TableRowField {
-  def apply[V](implicit f: TableRowField[V]): TableRowField[V] = f
-
-  def at[V](f: Any => V)(g: V => Any): TableRowField[V] = new TableRowField[V] {
-    override def fromField(v: Any): V = f(v)
-    override def toField(v: V): Any = g(v)
-  }
-
-  implicit val trfBool = at[Boolean](_.toString.toBoolean)(identity)
-  implicit val trfInt = at[Int](_.toString.toInt)(identity)
-  implicit val trfLong = at[Long](_.toString.toLong)(identity)
-  implicit val trfFloat = at[Float](_.toString.toFloat)(identity)
-  implicit val trfDouble = at[Double](_.toString.toDouble)(identity)
-  implicit val trfString = at[String](_.toString)(identity)
-  implicit val trfByteArray = at[Array[Byte]](
+  implicit val trtBool = at[Boolean](_.toString.toBoolean)(identity)
+  implicit val trtInt = at[Int](_.toString.toInt)(identity)
+  implicit val trtLong = at[Long](_.toString.toLong)(identity)
+  implicit val trtFloat = at[Float](_.toString.toFloat)(identity)
+  implicit val trtDouble = at[Double](_.toString.toDouble)(identity)
+  implicit val trtString = at[String](_.toString)(identity)
+  implicit val trtByteArray = at[Array[Byte]](
     x => BaseEncoding.base64().decode(x.toString))(
     x => BaseEncoding.base64().encode(x))
 
   import TimestampConverter._
-  implicit val trfInstant = at(toInstant)(fromInstant)
-  implicit val trfDate = at(toLocalDate)(fromLocalDate)
-  implicit val trfTime = at(toLocalTime)(fromLocalTime)
-  implicit val trfDateTime = at(toLocalDateTime)(fromLocalDateTime)
+  implicit val trtInstant = at(toInstant)(fromInstant)
+  implicit val trtDate = at(toLocalDate)(fromLocalDate)
+  implicit val trtTime = at(toLocalTime)(fromLocalTime)
+  implicit val trtDateTime = at(toLocalDateTime)(fromLocalDateTime)
 
-  implicit def trfOption[V](implicit f: TableRowField[V]): TableRowField[Option[V]] =
-    new TableRowField[Option[V]] {
-      override def fromField(v: Any): Option[V] = ???
-      override def toField(v: Option[V]): Any = ???
-      override def get(r: R, k: String): Option[V] =
-        Option(r.get(k)).map(f.fromField)
-      override def put(r: R, k: String, v: Option[V]): Unit =
-        v.foreach(x => r.put(k, f.toField(x)))
-    }
-
-  implicit def trfSeq[V, S[V]](implicit f: TableRowField[V],
-                               toSeq: S[V] => Seq[V],
-                               fc: FactoryCompat[V, S[V]]): TableRowField[S[V]] =
-    new TableRowField[S[V]] {
-      override def fromField(v: Any): S[V] = ???
-      override def toField(v: S[V]): Any = ???
-      override def get(r: R, k: String): S[V] = r.get(k) match {
-        case null => fc.newBuilder.result()
-        case xs: java.util.List[_] => fc.build(xs.asScala.iterator.map(f.fromField))
+  implicit def trfOption[V](implicit t: TableRowType[V]): TableRowType[Option[V]] = {
+    t match {
+      case t: TableRowRecord[V] => new TableRowRecord[Option[V]] {
+        override def from(r: R): Option[V] = ???
+        override def to(t: Option[V]): R = ???
       }
-      override def put(r: R, k: String, v: S[V]): Unit =
-        r.put(k, toSeq(v).map(f.toField).asJava)
+      case t: TableRowField[V] => new TableRowField[Option[V]](t.getFn)(t.putFn) {
+
+      }
     }
+  }
+//    new TableRowField[Option[V]]({ v =>
+//      f.
+//    })()
+//      override def fromField(v: Any): Option[V] = ???
+//      override def toField(v: Option[V]): Any = ???
+//      override def get(r: R, k: String): Option[V] =
+//        Option(r.get(k)).map(f.fromField)
+//      override def put(r: R, k: String, v: Option[V]): Unit =
+//        v.foreach(x => r.put(k, f.toField(x)))
+//    }
+
+//  implicit def trfSeq[V, S[V]](implicit f: TableRowField[V],
+//                               toSeq: S[V] => Seq[V],
+//                               fc: FactoryCompat[V, S[V]]): TableRowField[S[V]] =
+//    new TableRowField[S[V]] {
+//      override def fromField(v: Any): S[V] = ???
+//      override def toField(v: S[V]): Any = ???
+//      override def get(r: R, k: String): S[V] = r.get(k) match {
+//        case null => fc.newBuilder.result()
+//        case xs: java.util.List[_] => fc.build(xs.asScala.iterator.map(f.fromField))
+//      }
+//      override def put(r: R, k: String, v: S[V]): Unit =
+//        r.put(k, toSeq(v).map(f.toField).asJava)
+//    }
 }
--- core/src/main/scala/magnolia/data/Converter.scala
+++ core/src/main/scala/magnolia/data/Converter.scala
@@ -1,14 +1,16 @@
 package magnolia.data
 
+trait Converter[T, R] extends Serializable
+
 object Converter {
-  trait Record[T, R] extends Serializable {
-    protected def empty: R
-    protected def from(r: R): T
-    protected def to(t: T): R
+  trait Record[T, R] extends Converter[T, R] {
+    def empty: R
+    def from(r: R): T
+    def to(t: T): R
   }
 
-  trait Field[V, R] extends Record[V, R] {
+  trait Field[V, R] extends Converter[V, R] {
     def get(r: R, k: String): V
-    def put(r: R, k: String, v: V): Unit
+    def put(r: R, k: String, v: V): R
   }
 }
